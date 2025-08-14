e8cdd642eca54151cf6d73b307dbe6b00dde0b73
Oscar GKTest <oscargktest@gmail.com>
2025-08-14 23:26:35 +0100

    Add NSubstitute, Add mock-driven unit tests

--- OscarGKTest.Tests/Requests/RegisterSpeakerRequestHandlerTests.cs
+++ OscarGKTest.Tests/Requests/RegisterSpeakerRequestHandlerTests.cs
@@ -1,4 +1,6 @@
-﻿using OscarGKTest.Models;
+﻿using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using OscarGKTest.Models;
 using OscarGKTest.Models.Enums;
 using OscarGKTest.Repositories;
 using OscarGKTest.Requests;
@@ -11,7 +13,7 @@ public class RegisterSpeakerRequestHandlerTests
 
     public RegisterSpeakerRequestHandlerTests()
     {
-        Repository = new Repository();
+        Repository = Substitute.For<IRepository>();
     }
 
     private static RegisterSpeakerRequest CreateValidRequest()
@@ -21,7 +23,7 @@ public class RegisterSpeakerRequestHandlerTests
             FirstName = "first-name",
             LastName = "last-name",
             Email = "email",
-            Experience = 1,
+            Experience = 0,
             HasBlog = true,
             BlogUrl = "blog-url",
             Browser = new WebBrowser { Name = WebBrowser.BrowserName.Chrome, MajorVersion = 1 },
@@ -46,6 +48,96 @@ public class RegisterSpeakerRequestHandlerTests
         // Assert
         Assert.True(result.Success);
         Assert.NotNull(result.SpeakerId);
+    }
+
+    [Fact]
+    public void RegisterSpeakerRequestHandler_ValidRequest_SavesCorrectInformation()
+    {
+        // Arrange
+        var request = CreateValidRequest();
+
+        // Act
+        var result = RegisterSpeakerRequestHandler.RegisterSpeaker(request, Repository);
+
+        // Assert
+        // TODO: Assert too large, not ideal. Better to check these things individually?
+        Repository.Received(1).SaveSpeaker(Arg.Is<Speaker>(s =>
+            s.FirstName == request.FirstName &&
+            s.LastName == request.LastName &&
+            s.Email == request.Email &&
+            s.Exp == request.Experience &&
+            s.HasBlog == request.HasBlog &&
+            s.BlogURL == request.BlogUrl &&
+            s.Browser == request.Browser &&
+            s.Certifications.SequenceEqual(request.Certifications) &&
+            s.Employer == request.EmployerName &&
+            s.RegistrationFee == 500 &&
+            s.Sessions.Count == request.Sessions.Count &&
+            s.Sessions.All(session => session.Approved)
+        ));
+    }
+
+    // The below four tests that check the returned fee are somewhat flawed
+    // They cover each possible outcome of the switch statement, but don't cover the full ranges of the each condition
+    // Which we would need to do for full code coverage.
+    [Fact]
+    public void RegisterSpeakerRequestHandler_TwoYearsExperience_Returns250Fee()
+    {
+        // Arrange
+        var request = CreateValidRequest();
+        request.Experience = 2;
+        var expectedFee = 250;
+
+        // Act
+        var result = RegisterSpeakerRequestHandler.RegisterSpeaker(request, Repository);
+
+        // Assert
+        Repository.Received(1).SaveSpeaker(Arg.Is<Speaker>(s => s.RegistrationFee == expectedFee));
+    }
+
+    [Fact]
+    public void RegisterSpeakerRequestHandler_FourYearsExperience_Returns100Fee()
+    {
+        // Arrange
+        var request = CreateValidRequest();
+        request.Experience = 4;
+        var expectedFee = 100;
+
+        // Act
+        var result = RegisterSpeakerRequestHandler.RegisterSpeaker(request, Repository);
+
+        // Assert
+        Repository.Received(1).SaveSpeaker(Arg.Is<Speaker>(s => s.RegistrationFee == expectedFee));
+    }
+
+    [Fact]
+    public void RegisterSpeakerRequestHandler_SixYearsExperience_Returns50Fee()
+    {
+        // Arrange
+        var request = CreateValidRequest();
+        request.Experience = 6;
+        var expectedFee = 50;
+
+        // Act
+        var result = RegisterSpeakerRequestHandler.RegisterSpeaker(request, Repository);
+
+        // Assert
+        Repository.Received(1).SaveSpeaker(Arg.Is<Speaker>(s => s.RegistrationFee == expectedFee));
+    }
+
+    [Fact]
+    public void RegisterSpeakerRequestHandler_TenYearsExperience_ReturnsZeroFee()
+    {
+        // Arrange
+        var request = CreateValidRequest();
+        request.Experience = 10;
+        var expectedFee = 0;
+
+        // Act
+        var result = RegisterSpeakerRequestHandler.RegisterSpeaker(request, Repository);
+
+        // Assert
+        Repository.Received(1).SaveSpeaker(Arg.Is<Speaker>(s => s.RegistrationFee == expectedFee));
     }
 
     [Fact]
@@ -142,5 +234,22 @@ public class RegisterSpeakerRequestHandlerTests
         // Assert
         Assert.False(result.Success);
         Assert.Equal(RegisterError.NoSessionsApproved, result.Error);
+    }
+
+    [Fact]
+    public void RegisterSpeakerRequestHandler_SaveSpeakerThrows_ReturnsDatabaseFailureError()
+    {
+        // Arrange
+        var request = CreateValidRequest();
+        Repository
+            .SaveSpeaker(Arg.Any<Speaker>())
+            .Throws(new Exception());
+
+        // Act
+        var result = RegisterSpeakerRequestHandler.RegisterSpeaker(request, Repository);
+
+        // Assert
+        Assert.False(result.Success);
+        Assert.Equal(RegisterError.DatabaseFailure, result.Error);
     }
 }
